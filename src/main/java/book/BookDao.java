2b6df794021b573eb9ddf613c8cda07054630a86
Omaima Wasim Jilani <omaimajilani17@gmail.com>
2020-04-30 11:54:41 +0200

    created book package

---
+++ src/main/java/book/BookDao.java
@@ -0,0 +1,41 @@
+package book;
+
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+import java.util.Optional;
+
+@RegisterBeanMapper(Book.class)
+public interface BookDao {
+
+    @SqlUpdate("""
+            CREATE TABLE book(
+                    isbn13 VARCHAR PRIMARY KEY,
+                    author VARCHAR NOT NULL,
+                    title VARCHAR NOT NULL,
+                    format VARCHAR NOT NULL,
+                    publisher VARCHAR NOT NULL,
+                    publicationDate DATE NOT NULL,
+                    pages INTEGER NOT NULL,
+                    available BOOLEAN NOT NULL
+                    )
+            """)
+    void createTable();
+
+    @SqlUpdate("INSERT INTO book VALUES (:isbn13,:author,:title,:format,:publisher,:publicationDate,:pages,:available)")
+    void insert(@BindBean Book book);
+
+    @SqlQuery("SELECT * FROM book WHERE isbn13 =:isbn13")
+    Optional<Book> find(@Bind("isbn13") String isbn13);
+
+    @SqlUpdate("DELETE FROM book WHERE isbn13 =:isbn13")
+    void delete(@BindBean Book book);
+
+    @SqlQuery("SELECT * FROM book ORDER BY publicationDate")
+    List<Book> findAll();
+
+}
---
+++ src/main/java/book/Main.java
@@ -0,0 +1,33 @@
+package book;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:bookDb");
+        jdbi.installPlugin(new SqlObjectPlugin());
+
+        try(Handle handle = jdbi.open()){
+            BookDao bookDao = handle.attach((BookDao.class));
+            bookDao.createTable();
+
+            bookDao.insert(new Book("9781406360196","Anthony Horowitz",Book.Format.PAPERBACK,"Walker Books Ltd", LocalDate.parse("2015-05-05"),272,true));
+
+            bookDao.insert(new Book("9781406360240","Anthony Horowitz",Book.Format.PAPERBACK,"Walker Books Ltd", LocalDate.parse("2015-05-05"),384,false));
+
+            bookDao.insert(new Book("9781612620244","Hajime Isayama",Book.Format.PAPERBACK,"Kodansha America, Inc",LocalDate.parse("2012-06-19"),200,true));
+
+            bookDao.insert(new Book("9781421587660","Haruichi Furudate",Book.Format.PAPERBACK,"Viz Media, Subs. of Shogakukan Inc",LocalDate.parse("2016-07-28"),192,true));
+
+            bookDao.findAll().forEach(System.out::println);
+            bookDao.delete(bookDao.find("9781406360240").get());
+            bookDao.findAll().forEach(System.out::println);
+        }
+
+
+    }
+}
