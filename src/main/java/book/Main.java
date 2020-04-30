5b423ec30189a9660b058cc2b0430056a6c89198
Omaima Wasim Jilani <omaimajilani17@gmail.com>
2020-04-30 12:23:20 +0200

    Corrected a mistake

--- src/main/java/book/Main.java
+++ src/main/java/book/Main.java
@@ -15,13 +15,13 @@ public class Main {
             BookDao bookDao = handle.attach((BookDao.class));
             bookDao.createTable();
 
-            bookDao.insert(new Book("9781406360196","Anthony Horowitz",Book.Format.PAPERBACK,"Walker Books Ltd", LocalDate.parse("2015-05-05"),272,true));
+            bookDao.insert(new Book("9781406360196","Anthony Horowitz","Alex Rider:Stormbreaker",Book.Format.PAPERBACK,"Walker Books Ltd", LocalDate.parse("2015-05-05"),272,true));
 
-            bookDao.insert(new Book("9781406360240","Anthony Horowitz",Book.Format.PAPERBACK,"Walker Books Ltd", LocalDate.parse("2015-05-05"),384,false));
+            bookDao.insert(new Book("9781406360240","Anthony Horowitz","Alex Rider: Ark angel",Book.Format.PAPERBACK,"Walker Books Ltd", LocalDate.parse("2015-05-05"),384,false));
 
-            bookDao.insert(new Book("9781612620244","Hajime Isayama",Book.Format.PAPERBACK,"Kodansha America, Inc",LocalDate.parse("2012-06-19"),200,true));
+            bookDao.insert(new Book("9781612620244","Hajime Isayama","Attack on titan 1",Book.Format.PAPERBACK,"Kodansha America, Inc",LocalDate.parse("2012-06-19"),200,true));
 
-            bookDao.insert(new Book("9781421587660","Haruichi Furudate",Book.Format.PAPERBACK,"Viz Media, Subs. of Shogakukan Inc",LocalDate.parse("2016-07-28"),192,true));
+            bookDao.insert(new Book("9781421587660","Haruichi Furudate","Haikyuu 1",Book.Format.PAPERBACK,"Viz Media, Subs. of Shogakukan Inc",LocalDate.parse("2016-07-28"),192,true));
 
             bookDao.findAll().forEach(System.out::println);
             bookDao.delete(bookDao.find("9781406360240").get());
